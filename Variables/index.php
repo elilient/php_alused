36902c769de0a152a4f2bea1cf64aebe0a025762
edgar.lilienthal <edgar.lilienthal@khk.ee>
2018-12-13 15:27:50 +0200

    muutujad kasutamine

---
+++ Variables/index.php
@@ -0,0 +1,17 @@
+<?php
+// muutujate defineerimine
+// $muutujaNimi = väärtus;
+$taisArv = 7;
+$reaalArv = 3.14;
+$sone = 'kodu';
+$toevaartusTrue = 'true';
+$toevaartusFalse = 'false';
+
+echo 'Täisarv = '.$taisArv.'<br>';
+echo 'reaalArv = '.$reaalArv.'<br>';
+echo 'sone = '.$sone.'<br>';
+echo 'toevaartusTrue = '.$toevaartusTrue.'<br>';
+echo 'toevaartusFalse = '.$toevaartusFalse.'<br>';
+
+
+?>
--- print/index.php
+++ print/index.php
@@ -6,13 +6,9 @@
     <body>
     <h4>Väljastus</h4>
         <?php
-        // teksti väljastus print käsuga
-        print 'See on esimene lause PHP keeles'.'<br>';
-        print 'See on teine lause PHP keeles'.'<br>';
-
         // teksti väljastus echo käsuga
         echo 'See tekst on echo abil väljastatud'.'<br>'
-
         ?>
+
     </body>
 </html>
