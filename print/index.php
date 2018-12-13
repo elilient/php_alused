762492e41a035730df8668f568cd3c8e47a64df0
edgar.lilienthal <edgar.lilienthal@khk.ee>
2018-12-13 15:04:55 +0200

    echo ja print

---
+++ print/index.php
@@ -0,0 +1,9 @@
+<?php
+// teksti väljastus print käsuga
+print 'See on esimene lause PHP keeles'.'<br>';
+print 'See on teine lause PHP keeles'.'<br>';
+
+// teksti väljastus echo käsuga
+echo 'See tekst on echo abil väljastatud'.'<br>'
+
+?>
