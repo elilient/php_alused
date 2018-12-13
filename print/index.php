a8aaae647552b39d91dc2388c6717ec63f3ed439
edgar.lilienthal <edgar.lilienthal@khk.ee>
2018-12-13 15:09:01 +0200

    php ja html

--- print/index.php
+++ print/index.php
@@ -1,9 +1,18 @@
-<?php
-// teksti väljastus print käsuga
-print 'See on esimene lause PHP keeles'.'<br>';
-print 'See on teine lause PHP keeles'.'<br>';
+<!doctype html>
+<html>
+    <head>
+        <title>php alused - väljund</title>
+    </head>
+    <body>
+    <h4>Väljastus</h4>
+        <?php
+        // teksti väljastus print käsuga
+        print 'See on esimene lause PHP keeles'.'<br>';
+        print 'See on teine lause PHP keeles'.'<br>';
 
-// teksti väljastus echo käsuga
-echo 'See tekst on echo abil väljastatud'.'<br>'
+        // teksti väljastus echo käsuga
+        echo 'See tekst on echo abil väljastatud'.'<br>'
 
-?>
+        ?>
+    </body>
+</html>
