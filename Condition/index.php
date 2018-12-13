3e64b53559e437b9b502280d30664038622f1e61
edgar.lilienthal <edgar.lilienthal@khk.ee>
2018-12-13 16:00:01 +0200

    funktsioonid

---
+++ Condition/index.php
@@ -0,0 +1,18 @@
+<?php
+
+// tingimuslaused
+// tegevused, mis toimuvad, kui tingimus kehtib
+
+echo '<h4>Tingimuslaused</h4>';
+$arv = 2;
+
+if($arv % 2 == 0){
+    echo '<p style="color: green">'.$arv.'</p>';
+    else {
+        echo '<p style="color: red">';
+    }
+}
+
+
+
+?>
