2dfe35006d1b1b1679e5473eed3845f6736348d8
edgar.lilienthal <edgar.lilienthal@khk.ee>
2018-12-13 15:54:33 +0200

    võrdlusoperaatorid

--- Variables/index.php
+++ Variables/index.php
@@ -3,7 +3,7 @@
 // $muutujaNimi = väärtus;
 $taisArv = 7;
 $reaalArv = 3.14;
-$sone = 'kodu';
+$sone = '7';
 $toevaartusTrue = 'true';
 $toevaartusFalse = 'false';
 
@@ -12,6 +12,26 @@ echo 'reaalArv = '.$reaalArv.'<br>';
 echo 'sone = '.$sone.'<br>';
 echo 'toevaartusTrue = '.$toevaartusTrue.'<br>';
 echo 'toevaartusFalse = '.$toevaartusFalse.'<br>';
+echo gettype($sone).'<br>';
+settype($sone, 'int');
+echo gettype($sone).'<br>';
+$arv1 = 7;
+$arv2 = 24;
+
+echo $arv1.' + '.$arv2.' = '.($arv1 + $arv2).'<br>';
+echo $arv1.' - '.$arv2.' = '.($arv1 - $arv2).'<br>';
+echo $arv1.' * '.$arv2.' = '.($arv1 * $arv2).'<br>';
+echo $arv1.' / '.$arv2.' = '.($arv1 / $arv2).'<br>';
+echo $arv1.' % '.$arv2.' = '.($arv1 % $arv2).'<br>';
+
+echo '<hr>';
+echo '<h4>Võrdlusoperaatorid</h4>';
+echo $arv1.' < '.$arv2.' see on '.($arv1 < $arv2).'<br>';
+echo $arv1.' > '.$arv2.' see on '.($arv1 > $arv2).'<br>';
+echo $arv1.' <= '.$arv2.' see on '.($arv1 <= $arv2).'<br>';
+echo $arv1.' >= '.$arv2.' see on '.($arv1 >= $arv2).'<br>';
+echo $arv1.' == '.$arv2.' see on '.($arv1 == $arv2).'<br>';
+echo $arv1.' != '.$arv2.' see on '.($arv1 != $arv2).'<br>';
 
 
 ?>
