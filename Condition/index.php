54ca0138d1e52f0de71be031db954d1030bfef19
edgar.lilienthal <edgar.lilienthal@khk.ee>
2018-12-13 16:12:15 +0200

    funktsioonid ja värvimuutus.

--- Condition/index.php
+++ Condition/index.php
@@ -4,14 +4,23 @@
 // tegevused, mis toimuvad, kui tingimus kehtib
 
 echo '<h4>Tingimuslaused</h4>';
-$arv = 2;
+$arv = rand(0, 100);
 
-if($arv % 2 == 0){
-    echo '<p style="color: green">'.$arv.'</p>';
-    else {
-        echo '<p style="color: red">';
-    }
+if($arv  > 0 and $arv < 26) {
+    $varv = 'green';
+
+} else if($arv > 25 and $arv < 51){
+    $varv = 'red';
+
+} else if($arv > 50 and $arv < 76){
+    $varv = 'blue';
+
+} else if($arv > 75 and $arv < 101){
+    $varv = 'orange';
+
 }
+echo '<p style="color: '.$varv.'">'.$arv.'</p>';
+
 
 
 
