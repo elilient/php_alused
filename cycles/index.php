41eaad2bd63259add80bb880594f3b58942c89f7
edgar.lilienthal <edgar.lilienthal@khk.ee>
2018-12-14 10:10:14 +0200

    värviline tabel

--- cycles/index.php
+++ cycles/index.php
@@ -1,4 +1,5 @@
 <?php
+header('Refresh: 1');
 echo '<h4>Tsüklid - for</h4>';
 $varv = '';
 for($arv = 1; $arv <= 10; $arv++){
@@ -9,5 +10,20 @@ for($arv = 1; $arv <= 10; $arv++){
     }
     echo '<div style="color: '.$varv.'">'.$arv.'</div>';
 }
+echo '<table>';
+for($rida = 1; $rida <= 5; $rida++){
+    echo '<tr>';
+    for($veerg = 1; $veerg <= 5; $veerg++) {
+        $varv = '#';
+        for($kord = 1; $kord <= 6; $kord++){
+            $varv = $varv.dechex(rand(0,15));
+        }
+        echo '<td style="background: ' . $varv . '">';
+        echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+        echo '</td>';
+    }
+    echo '</tr>';
+}
+echo '</table>';
 
 ?>
