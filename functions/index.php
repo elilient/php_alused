d7d82935adcb69401cc600b937e4af1b47f79707
edgar.lilienthal <edgar.lilienthal@khk.ee>
2018-12-14 11:42:23 +0200

    mitu korda esineb arv funktsioon

--- functions/index.php
+++ functions/index.php
@@ -1,7 +1,24 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: edgar.lilienthal
- * Date: 14.12.2018
- * Time: 11:31
+ * function funktsiooniNimi($param1, $param2, ..., $paramN) {
+ * tegevused, mis toimuvad funktsiooni sees
+ * }
  */
+$arv = 4421587455555;
+function numbriKordumine($arv, $kontrollNumber)
+{
+echo 'Arv on '.$arv.'<br>';
+$numberKokku = 0;
+// leia, mitu korda siin esinemas number 5;
+while($arv != 0) {
+    $number = $arv % 10;
+    if($number == $kontrollNumber) {
+        $numberKokku = $numberKokku +1; //numberKokku++
+    }
+    $arv = $arv / 10;
+    settype($arv, int);
+}
+echo 'Number '.$kontrollNumber.' esineb antud arvus '.$numberKokku.'<br>';
+}
+numbriKordumine(4421587455555, 5);
+numbriKordumine(4421587455555, 4);
