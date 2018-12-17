16ad3c049646c5b4671e112a1b84e98c028e8536
edgar.lilienthal <edgar.lilienthal@khk.ee>
2018-12-17 12:59:35 +0200

    Funktsiooni väljakutsumine

--- functions/index.php
+++ functions/index.php
@@ -1,24 +1,12 @@
+<html>
+<head>
+    <title>Funktsiooni väljakutsumine</title>
+</head>
+<body>
 <?php
-/**
- * function funktsiooniNimi($param1, $param2, ..., $paramN) {
- * tegevused, mis toimuvad funktsiooni sees
- * }
- */
-$arv = 4421587455555;
-function numbriKordumine($arv, $kontrollNumber)
-{
-echo 'Arv on '.$arv.'<br>';
-$numberKokku = 0;
-// leia, mitu korda siin esinemas number 5;
-while($arv != 0) {
-    $number = $arv % 10;
-    if($number == $kontrollNumber) {
-        $numberKokku = $numberKokku +1; //numberKokku++
-    }
-    $arv = $arv / 10;
-    settype($arv, int);
-}
-echo 'Number '.$kontrollNumber.' esineb antud arvus '.$numberKokku.'<br>';
-}
-numbriKordumine(4421587455555, 5);
-numbriKordumine(4421587455555, 4);
+$num = - 321;
+$newnum = abs($num);
+print $newnum;
+?>
+</body>
+</html>
