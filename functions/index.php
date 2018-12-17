30cc66dcfe3052ed0a1c254f768b7f7fb076007f
edgar.lilienthal <edgar.lilienthal@khk.ee>
2018-12-17 13:19:23 +0200

    Muutuja väärtuse sälitimae global instruktsiooni abil.

--- functions/index.php
+++ functions/index.php
@@ -1,15 +1,19 @@
 <html>
 <head>
-    <title>Muutuja poole pöörudmine global instruktsiooni abil</title>
+    <title>Muutuja väärtuse säilitamine global instruktsiooni abil.</title>
 </head>
 <body>
 <?php
-$life = 42;
-function meaningOfLife() {
-    global $life;
-    print "The meaning of life is $life<br>";
+$num_of_calls = 0;
+function andAnotherThing($txt) {
+    global $num_of_calls;
+    $num_of_calls++;
+    print "<h1>$num_of_calls. $txt</h1>";
 }
-meaningOfLife();
+andAnotherThing("Widgets");
+print("We build a fine range of widgets<p>");
+andAnotherThing("Doodads");
+print("Finest in the world<p>");
 ?>
 </body>
 </html>
