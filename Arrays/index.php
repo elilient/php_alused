888350f7e4d0f0224caa7ccb7d1450a283da1c2d
edgar.lilienthal <edgar.lilienthal@khk.ee>
2018-12-18 12:53:35 +0200

    Massiiv - tabel poolik

--- Arrays/index.php
+++ Arrays/index.php
@@ -13,8 +13,15 @@ array(
         'male'
     )
 );
-foreach ($kasutajad as $kasutaja){
-    foreach ($kasutaja as $info) {
+
+echo '<table>';
+for ($i = 0; $i < count($kasutajad); $i++){
+    if($kasutajad[$i][3] == 'female'){
+        echo '<th>'.$kasutajad[$i][3].'</th>';
+    } else {
+
+    }
+    foreach ($kasutajad[$i] as $info) {
         echo $info.'<br>';
     }
     echo '<hr>';
