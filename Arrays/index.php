6a1299637703b7b1380331015983b13fcbdb47da
edgar.lilienthal <edgar.lilienthal@khk.ee>
2018-12-18 12:17:47 +0200

    Massiiv - väljastamine ja elementide töötlus

--- Arrays/index.php
+++ Arrays/index.php
@@ -1,15 +1,23 @@
 <?php
-$kasutaja = array(); //Tühi massiiv
+$kasutajad = array(); //Tühi massiiv
 // andmete lisamine
 $kasutajad[] = 'alice';
 $kasutajad[] = 'bob';
 array_push($kasutajad, 'lucy');
-
-//andmete massiiv
 $lisaKasutajad = array('mark', 'george');
-echo '<pre>';
-print_r($kasutajad);
-print_r($lisaKasutajad);
-echo '</pre>';
-//var_dump($kasutaja);
+
+$kasutajadKokku = count($kasutajad);
+for($i=0; $i < $kasutajadKokku; $i++){
+    echo $kasutajadKokku[$i].'<br>';
+}
+
+array_shift($kasutajad);
+for($i = 0; $i < $kasutajadKokku; $i++) {
+    echo $kasutajad[$i].'<br>';
+}
+
+$uuedKasutajad = array_merge($kasutajad, $lisaKasutajad);
+for($i = 0; $i < $uuedKasutajad; $i++) {
+    echo $uuedKasutajad[$i].'<br>';
+}
 ?>
