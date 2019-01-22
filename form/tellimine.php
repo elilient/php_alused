c900ad868543b322bce80e3b9a05248f8bdd16fd
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-01-22 11:31:11 +0200

    Vorm input ja kera arvutus

--- form/tellimine.php
+++ form/tellimine.php
@@ -1,11 +1,12 @@
 <h1>Tellimine</h1>
 <?php
 //lisab vormist saadud andmed muutujasse
-$toode1 = $_GET['t1'];
+$ring = $_GET['ring'];
+$pii = 3.14;
 $toode2 = $_GET['t2'];
 $toode3 = $_GET['t3'];
 
-echo 'Toode 1: '.$toode1.'tk<br>';
+echo 'Kera ruumala: '.$pii*$ring*$ring*$ring.'<br>';
 echo 'Toode 2: '.$toode2.'tk<br>';
 echo 'Toode 3: '.$toode3.'tk<br>';
-?>
+?>.
