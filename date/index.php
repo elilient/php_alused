fa382916a18a2024321abc7548a4f378b421fb6a
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-01-22 12:21:03 +0200

    Date - päevad ja kuud eesti keeles

--- date/index.php
+++ date/index.php
@@ -1,10 +1,55 @@
+<h1>Ülesanne 8</h1>
 <?php
-//kuude massiiv
-$eesti_kuud = array(1=>'jaanuar', 'veebruar', 'märts', 'aprill', 'mai', 'juuni', 'juuli', 'august', 'september', 'oktoober', 'november', 'detsember');
-//kuupäevad massiividesse
-$paev = date('d');
-$kuu = $eesti_kuud[date('n')];
-$aasta = date('Y');
-//kuupäeva väljastamine
-echo $paev.'.'.$kuu.' '.$aasta;	//22.veebruar2013
+echo date('d.m.Y G:i' , time());
+echo '<br>';
+function paev() {
+    if(date('l')=='Tuesday'){
+        echo('teisipäev');
+    } elseif (date('l')=='Monday'){
+        echo('esmaspäev');
+    } elseif (date('l')=='Wednesday') {
+        echo('kolmapäev');
+    }  elseif (date('l')=='Thursday') {
+        echo('neljapäev');
+    }  elseif (date('l')=='Friday') {
+        echo('reede');
+    }  elseif (date('l')=='Saturday') {
+        echo('laupäev');
+    }  else {
+        echo('pühapäev');
+    }
+}
+
+function kuu(){
+    if(date('F')=='January'){
+        echo('jaanuar');
+    } elseif (date('F')=='February') {
+        echo('veebruar');
+    } elseif (date('F')=='March') {
+        echo('märts');
+    } elseif (date('F')=='April') {
+        echo('aprill');
+    } elseif (date('F')=='May') {
+        echo('mai');
+    } elseif (date('F')=='June') {
+        echo('Juuni');
+    } elseif (date('F')=='July') {
+        echo('juuli');
+    } elseif (date('F')=='August') {
+        echo('august');
+    } elseif (date('F')=='September') {
+        echo('september');
+    } elseif (date('F')=='October') {
+        echo('oktoober');
+    } elseif (date('F')=='November') {
+        echo('november');
+    } elseif (date('F')=='December') {
+        echo('detsember');
+    }
+}
+echo paev();
+echo '<br>';
+echo date('d.');
+echo kuu().' '.date('Y').' ';
+echo paev();
 ?>
