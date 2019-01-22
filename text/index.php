52fd0322d010bd78d896eec0704092453e8fe1cc
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-01-22 13:18:22 +0200

    Text - str wordcount

--- text/index.php
+++ text/index.php
@@ -1,9 +1,6 @@
 <?php
 
 $tekst = 'All thinking men are atheists';
-echo substr($tekst, 3, 5);		//thin
-echo '<br>';
-echo substr($tekst, 4, -13);	//thinking men
-echo '<br>';
-echo substr($tekst, -8, 7);		//atheist
+print_r(str_word_count($tekst, 2));
+//Array ( [0] => All [4] => thinking [13] => men [17] => are [21] => atheists )
 ?>
