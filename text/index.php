40c5dc7c0d7e3b1bb0fa1405ff725a046ab0d4fc
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-01-22 13:17:14 +0200

    Text - teksti pikkus

--- text/index.php
+++ text/index.php
@@ -1,6 +1,7 @@
 <?php
 
-$tekst = 'Life Is About Ignoring The Drama.';
-
-echo ucfirst(strtolower($tekst));
+$tekst = 'Experience is the teacher of fools';
+echo strlen($tekst);			//34
+echo '<br>';
+echo str_word_count($tekst);	//6
 ?>
