93d3f0b7edbda864347710240f1b74ba35bd5e65
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-01-22 11:47:15 +0200

    Date - kuupäeva eestistamine

--- date/index.php
+++ date/index.php
@@ -1,3 +1,10 @@
 <?php
-date_default_timezone_set('Europe/Tallinn');	//22.02.2013 18:02
+//kuude massiiv
+$eesti_kuud = array(1=>'jaanuar', 'veebruar', 'märts', 'aprill', 'mai', 'juuni', 'juuli', 'august', 'september', 'oktoober', 'november', 'detsember');
+//kuupäevad massiividesse
+$paev = date('d');
+$kuu = $eesti_kuud[date('n')];
+$aasta = date('Y');
+//kuupäeva väljastamine
+echo $paev.'.'.$kuu.' '.$aasta;	//22.veebruar2013
 ?>
