f2c63b138529aa7f362208e9d4ad0ccc6b26419a
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-01-22 12:59:56 +0200

    Date - maailmalõpp

--- date/index.php
+++ date/index.php
@@ -67,5 +67,6 @@ echo '<br>';
 echo 'Minu sünnipäev on 24.01.1993';
 echo '<br>';
 echo 'Isik A on minust vanem '.round($synnipvahe / (60*60*24*365)).' aastat';
-
+echo '<br>';
+echo 'Maailmalõpp saabub 19.02.2016. Hetke kuupäev on '.date('d.m.Y').' See ei ole võimalik';
 ?>
