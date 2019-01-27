dff3b7fc24c0607edca7030562d4e0129851ec24
Edgar <edgar.lilienthal@khk.ee>
2019-01-27 21:19:12 +0200

    Recode - index.php

---
+++ recode/index.php
@@ -0,0 +1,25 @@
+<?php include('pais.php'); ?>
+    <menu>
+        <a href="index.php">Avaleht</a>
+        <a href="index.php?leht=portfoolio">Portfoolio</a>
+        <a href="index.php?leht=kaart">Kaart</a>
+        <a href="index.php?leht=kontakt">Kontakt</a>
+    </menu>
+<?php
+if(!empty($_GET['leht'])){
+    $leht = htmlspecialchars($_GET['leht']);
+    $lubatud = array('portfoolio','kaart','kontakt');
+    $kontroll = in_array($leht, $lubatud);
+    if($kontroll==true){
+        include($leht.'.php');
+    } else {
+        echo 'Valitud lehte ei eksisteeri!';
+    }
+} else{
+    ?>
+    <h2>Lorem ipsum</h2>
+    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lacinia feugiat mi, ac blandit purus hendrerit vel.</p>
+    <?php
+}
+?>
+<?php include('jalus.php'); ?>
