4c0bd4da4683e2e238e01f89a1c9696c142380e5
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-01-28 12:05:47 +0200

    session - loodud sisselogimisvorm

---
+++ recode/meist.php
@@ -0,0 +1,2 @@
+<h2>Meist</h2>
+<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lacinia feugiat mi, ac blandit purus hendrerit vel.</p>
---
+++ recode/pais.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+<head>
+    <title>Koodi taaskasutamine</title>
+</head>
+<body>
+<header id="pais">
+    <h1>Lorem Ipsum web</h1>
+</header>
