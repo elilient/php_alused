691caf7743fab0906bf1de3247a2c0ea76197cbc
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-01-28 12:20:53 +0200

    sessioon - loodud kasutaja sessioon

--- session/acts/login.php
+++ session/acts/login.php
@@ -4,11 +4,14 @@ require_once '../db/db_fnk.php';
 // andmed HTML vormi poolt
 $username = $_GET['uname'];
 $password = $_GET['upass'];
-echo $username.' '.$password;
 // loome ühendus andmebaasiga
 $ikt = connect_db(DBHOST, DBUSER, DBPASS, DBNAME);
 // küsime kasutaja andmete olemasolu andmebaasis
 $sql = 'SELECT * FROM user WHERE username="'.$username.'" AND password="'.md5($password).'"';
 $users = getData($sql, $ikt);
-// väljastame andmed
-echo $users[0]['first_name'].' '.$users[0]['last_name'];
+// loome kasutaja sessiioon, kui ta andmebaasis olemas
+if($users !== false){
+    session_start();
+    $_SESSION['user'] = $users[0];
+    header('Location: ../index.php');
+}
